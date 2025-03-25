44b9f2e473df4a6cbf482d8c2a8022cbdd2dba29
c4ar0lin4 <car0lzinh4rc@gmail.com>
2025-03-25 19:05:26 +0000

    criei

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+var nome = "carolina" //
+let idade = 17 //
+const signo = "sagitario" //
+
+/*
+Variavel q muda o a valor na execução do codigo
+variavel q muda o valor na execução do codigo, pode ter duas variaveis com o mesm codigio
+varaiavel consatnte não muda o valor durante a execução
+*/
+
